c067d773701290c0a3f43892799cd0146c304952
王鑫 <jiugewx@qq.com>
2017-03-11 15:57:50 +0800

    封装了打包的entry，更多注重页面的路由部分

--- admin/entry/entry-base.js
+++ admin/entry/entry-base.js
@@ -5,8 +5,29 @@
 // 处理资源文件的加载，以及基础组件的加载；（这是一个面向切面的过程）
 import "../../src/style/style.less";
 import "../../src/utils/index";
-import App from "./App.vue"
+import App from "./App.vue";
+import Vue from "vue";
+import VueRouter from "vue-router";
 // import wx from ""  实例化基础组件
 
+Vue.use(VueRouter);
 
-export default App;
+Vue.config.productionTip = false;
+
+function start(routerMap) {
+    var router = new VueRouter({
+        routes: routerMap
+    });
+
+
+    new Vue({
+        el: '#admin_app',
+        router: router,
+        template: '<App/>',
+        components: {App}
+    });
+}
+
+export default {
+    start : start,
+}
--- admin/views/page2/main.js
+++ admin/views/page2/main.js
@@ -2,22 +2,11 @@
  * Created by wangxin on 2017/3/7.
  */
 
-import App from "../../entry/App.vue";
-import Vue from "vue";
-import VueRouter from "vue-router";
+import base from "../../entry/entry-base";
 
-Vue.config.productionTip = false;
-Vue.use(VueRouter);
-
-
-var router = new VueRouter({
-    routes: []
-});
+var routerMap = [
+    {}
+];
 
 
-new Vue({
-    el: '#admin_app',
-    router: router,
-    template: '<App/>',
-    components: {App}
-});
+base.start(routerMap);
